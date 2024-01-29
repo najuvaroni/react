b396ecaee91b3ccdb13de42fad23d5f98e364cf6
najuvaroni <124930475+najuvaroni@users.noreply.github.com>
2024-01-29 17:14:20 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import Despesas from "./components/Despesas";
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
-function App() {
-    return (
-        <div className="App">
-            <Despesas></Despesas>
-        </div>
-    );
+export default function App() {
+  const [click, setClick] = useState(0);
+
+  const QuandoClicar = () => {
+    setClick(click + 1);
+  };
+  const Remover = () =>{
+    setClick(click - 1)
+  }
+
+  return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Senai</Text>
+
+        <Button
+            style={styles.btt}
+            title={`Clique aqui (${click} vezes)`}
+            onPress={QuandoClicar}
+        />
+        <Button
+            style={styles.btt}
+            title={`Clique aqui para remover)`}
+            onPress={Remover}
+        />
+
+        <Text style={styles.clickText}>{`Você clicou ${click} vezes`}</Text>
+
+        <StatusBar style="light" backgroundColor={'red'} translucent={false} />
+      </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+    color: 'pink',
+  },
+  btt: {
+    backgroundColor: 'pink',
+  },
+  clickText: {
+    marginTop: 20,
+    fontSize: 18,
+    color: 'white',
+  },
+});
